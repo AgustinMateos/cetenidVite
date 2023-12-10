7a895cb537d5580a447f76f9ccd8e7d8d3b94eba
agustin mateos <agustinrotondomateos@gmail.com>
2023-12-10 18:22:24 -0300

    comotrabajamos

--- src/components/ComoTrabajamosBody/ComoTrabajamosBody.jsx
+++ src/components/ComoTrabajamosBody/ComoTrabajamosBody.jsx
@@ -4,14 +4,14 @@ import comoTrabajamos16 from '../../assets/interfaz/comoTrabajamos16.jpg'
 const ComoTrabajamosBody = () => {
     return (
         <section>
-            <div className='comoTrabajamosContainerPrincipal'>
-                <div className='comoTrabajamosPrimerContainer'> 
-                    <div className='comoTrabajamosPrimerContainerImg'>
-                        <img className='comoTrabajamosImg' src={comoTrabajamos16} alt="" />
+            <div >
+                <div className='comoTrabajamosContainer' > 
+                    <div className='comoTrabajamosContainerImg'>
+                        <img  src={comoTrabajamos16} alt="" />
                     </div>
-                    <div>
+                    <div className='comoTrabajamosContainerTexto'>
                         <h1>Como Trabajamos</h1>
-                        <p>Nuestro CET brinda una atención personalizada
+                        <p >Nuestro CET brinda una atención personalizada
                         (individual y grupal) de lunes a viernes, de 8.30 a 15.30 hs.
                         en Haedo mediante grupos reducidos de similares características y
                         edades, a cargo de una Orientadora (profesional del área de la salud)

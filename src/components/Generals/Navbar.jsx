8d292cc2ba9e454fdb57dbb3bf5a56022b9de424
agustin mateos <agustinrotondomateos@gmail.com>
2023-12-11 20:51:40 -0300

    navbar contacto link

--- src/components/Generals/Navbar.jsx
+++ src/components/Generals/Navbar.jsx
@@ -142,12 +142,12 @@ export default function Navbar() {
                 >
                   Galeria
                 </a>
-                <a
-                  href="#"
+                <Link
+                 to={"/Contacto"}
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Contacto
-                </a>
+                </Link>
               </div>
               
             </div>

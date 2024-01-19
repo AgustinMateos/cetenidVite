3c7813d2cbc4e0dba1ec5c9bb278852ecb64b36a
agustin mateos <agustinrotondomateos@gmail.com>
2024-01-18 22:02:26 -0300

    galeria

--- src/components/GaleriaBody/GaleriaBody.jsx
+++ src/components/GaleriaBody/GaleriaBody.jsx
@@ -1,68 +1,69 @@
-
-import React, { useEffect, useState } from 'react';
-import neneJugando from '../../assets/interfaz/18.jpg'
-import nenePintando from '../../assets/interfaz/26.jpg'
-import neneDibujando from '../../assets/interfaz/20.jpg'
-import nenesInteractuando from '../../assets/slider/sliderP4.jpg'
-import nenesSentados from '../../assets/slider/sliderP9.jpg'
-import nenesBailando from '../../assets/slider/sliderP12.jpg'
-import nenesBailando2 from '../../assets/interfaz/comoTrabajamos2.jpg'
-import neneHablandoTelefono from '../../assets/interfaz/17.jpg'
-import neneBeso from '../../assets/interfaz/2.jpg'
-import ModalCard from './ModalCard';
-
-
-
-  const GaleriaBody = () => {
-
-    const [ imagenes , setImagenes ] = useState([]);
-
-    useEffect(() => {
-      setImagenes([]);
-    }, [] );
-
-    const imagenes1 = [ { id:1 , img:nenePintando },{ id:2 , img:nenesInteractuando }, { id:3 , img:nenesSentados  } ];
-    const imagenes2 = [ { id:2 , img:nenesInteractuando }, { id:3 , img:nenesSentados  } , { id:1 , img:nenePintando }  ];
-
-    
-
-    return (
-    <div className='galeriaBodyContainer'> {/* You can open the modal using document.getElementById('ID').showModal() method */}
-      <button className="galeriaBodyCards" onClick={() => {
-                                                            setImagenes( imagenes1 );
-                                                            document.getElementById('my_modal_3').showModal()}}>
-        <img src={nenePintando}/>
-      </button>
-      <button className="galeriaBodyCards" onClick={() => { 
-                                                            setImagenes( imagenes2 );
-                                                            document.getElementById('my_modal_3').showModal() }}>
-        <img src={nenesInteractuando}/>
-      </button>
-      <button className="galeriaBodyCards" onClick={() => document.getElementById('my_modal_3').showModal()}><img src={nenesSentados}/></button>
-      <button className="galeriaBodyCards" onClick={() => document.getElementById('my_modal_3').showModal()}><img src={neneDibujando}/></button>
-      <button className="galeriaBodyCards" onClick={() => document.getElementById('my_modal_3').showModal()}><img src={nenesBailando2}/></button>
-      <button className="galeriaBodyCards" onClick={() => document.getElementById('my_modal_3').showModal()}><img src={nenesBailando}/></button>
-      <button className="galeriaBodyCards" onClick={() => document.getElementById('my_modal_3').showModal()}><img src={neneJugando}/></button>
-      <button className="galeriaBodyCards" onClick={() => document.getElementById('my_modal_3').showModal()}><img src={neneHablandoTelefono}/></button>
-      <button className="galeriaBodyCards" onClick={() => document.getElementById('my_modal_3').showModal()}><img src={neneBeso}/></button>
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-          </form>
-          <div className="carousel w-full">
-            {
-              imagenes.map( imagen => <ModalCard key={ imagen.id } {...imagen }/>)
-            }
+import React from 'react'
+import logoCetenidSimple from '../../assets/iconos/logoComoTrabajamos.png'
+import nenesjugandoCetenid from '../../assets/slider/sliderP13.jpg'
+import neneTelefono from '../../assets/interfaz/17.jpg'
+import neneBailandoCetenid from '../../assets/slider/sliderP13.jpg'
+import neneDisfrazadoCetenid from '../../assets/slider/sliderP12.jpg'
+import nenesCetenid from '../../assets/slider/sliderP4.jpg'
+import nenaJugandoCetenid from '../../assets/interfaz/23.jpg'
+import nenePintandoCetenid from '../../assets/interfaz/2.jpg'
+import nenaCetenid from '../../assets/interfaz/18.jpg'
+import CetenidEquipo from "../../assets/interfaz/CetenidEquipo.jpeg"
+import cetenidTerapia from '../../assets/interfaz/15.jpg'
+import cetenidPileta from '../../assets/interfaz/homecard30.jpg'
+const GaleriaBody = () => {
+  return (
+    <div className="galeriaContainer">
+      <div className="galeriaContainerTitulo">
+        <h1 className='galeriaTitulo'>Galería Cetenid</h1>
+      </div>
+      <div className='galeriaContainerSliders'>
+        <div className='galeriaSliders'>
+          <div className="carousel  rounded-box">
+            <div className="carousel-item galeriaContainerImg">
+              <img src={neneTelefono} alt="Burger" />
+            </div>
+            
+            <div className="carousel-item galeriaContainerImg">
+              <img src={neneDisfrazadoCetenid} alt="Burger" />
+            </div>
+            <div className="carousel-item galeriaContainerImg">
+              <img src={nenesCetenid} alt="Burger" />
+            </div>
+            <div className="carousel-item galeriaContainerImg">
+              <img src={nenaJugandoCetenid} alt="Burger" />
+            </div>
+            <div className="carousel-item galeriaContainerImg">
+              <img src={nenePintandoCetenid} alt="Burger" />
+            </div>
+            
+          </div>
+          <div className="carousel rounded-box">
+            <div className="carousel-item galeriaContainerImg ">
+              <img src={nenaCetenid} alt="Burger" />
+            </div>
+            <div className="carousel-item galeriaContainerImg">
+              <img src={CetenidEquipo} alt="Burger" />
+            </div>
+            <div className="carousel-item galeriaContainerImg">
+              <img src={cetenidTerapia} alt="Burger" />
+            </div>
+            <div className="carousel-item galeriaContainerImg">
+              <img src={cetenidPileta} alt="Burger" />
+            </div>
+            <div className="carousel-item galeriaContainerImg">
+              <img src={neneBailandoCetenid} alt="Burger" />
+            </div>
             
           </div>
         </div>
-      </dialog>
-      
-      
-    </div>
-  );
-};
 
-export default GaleriaBody;
+      </div>
+      <div className='galeriaContainerTituloSecundario'><img className='galeriaContainerTituloSecundarioImg' src={logoCetenidSimple} alt="" /><h2 className="galeriaTituloSecundario">Jugamos, aprendemos y nos divertimos...</h2><img className="galeriaContainerTituloSecundarioImg" src={logoCetenidSimple} alt="" /></div>
+      <div className='galeriaContainerJugamosImg'><img src={nenesjugandoCetenid} alt="" /></div>
+    </div>
+
+  )
+}
+
+export default GaleriaBody
